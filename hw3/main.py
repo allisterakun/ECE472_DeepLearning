54891013ef7a4b302d85fe1879ad18a0eb68df34
allister <allisterakun@gmail.com>
2022-09-16 14:58:07 -0400

    add hw2

---
+++ hw3/main.py
@@ -0,0 +1,14 @@
+#!/bin/env python3.8
+
+"""
+Allister Liu
+"""
+
+import sys
+
+import matplotlib.pyplot as plt
+import numpy as np
+import tensorflow as tf
+
+from absl import flags
+from tqdm import trange
